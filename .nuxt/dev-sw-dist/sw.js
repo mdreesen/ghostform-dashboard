d2e0282abf6cb73399f3d5a83b7f53ab807e048c
mdreesen <mdreesen@stratacache.com>
2026-08-19 09:15:11 -0600

    feature(Update ghostform)

--- .nuxt/dev-sw-dist/sw.js
+++ .nuxt/dev-sw-dist/sw.js
@@ -78,7 +78,7 @@ define(['./workbox-b6ffa576'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "/login",
-    "revision": "0.ph7q2pia41"
+    "revision": "0.rqojnm8ihpo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/login"), {
