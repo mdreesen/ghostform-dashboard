035e6631e9975ff4fad2b36a507bd579567171bb
mdreesen <mdreesen@stratacache.com>
2026-08-20 07:41:38 -0600

    build

--- .nuxt/dev-sw-dist/sw.js
+++ .nuxt/dev-sw-dist/sw.js
@@ -78,7 +78,7 @@ define(['./workbox-b6ffa576'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "/login",
-    "revision": "0.j2da1s21hrg"
+    "revision": "0.phhjelq511o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/login"), {
